3dc11d6e7771072fc11c70d7ebf3c5dc92e6214e
sedykhaleks <sedykhaleks@gmail.com>
2022-06-08 14:03:52 +0300

    The app outputs 70 names

---
+++ SQL/name_x_while.java
@@ -0,0 +1,11 @@
+public class name_x_while {
+    public static void main (String[] args) {
+
+        int x = 0;
+        while (x < 70) {
+            x++;
+
+            System.out.println(("(" + "'" + "name_" + x + "'" + "),"));
+        }
+    }
+}
