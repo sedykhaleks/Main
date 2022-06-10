f706a121ebc7b712a5a342607c26f7334d4bd6e3
sedykhaleks <sedykhaleks@gmail.com>
2022-06-10 19:34:18 +0300

    Update file

--- SQL/HW2_SQL.sql
+++ SQL/HW2_SQL.sql
@@ -1,144 +1,150 @@
-/*SQL_DDL. Первая часть.*/
+SQL_DDL. Первая часть.
 
-/*Таблица employees*/
+Таблица employees
 
-/*1) Создать таблицу employees
+1) Создать таблицу employees
 - id. serial,  primary key,
-- employee_name. Varchar(50), not null */
+- employee_name. Varchar(50), not null
 
 CREATE TABLE employees(
-	ID SERIAL PRIMARY KEY,
+	id SERIAL PRIMARY KEY,
 	employee_name VARCHAR(50)
 );
 
-/*2) Наполнить таблицу employee 70 строками.*/
+2) Наполнить таблицу employee 70 строками.
 
 INSERT INTO employees (employee_name)
-VALUES ('name_1'),
-('name_2'),
-('name_3'),
-('name_4'),
-('name_5'),
-('name_6'),
-('name_7'),
-('name_8'),
-('name_9'),
-('name_10'),
-('name_11'),
-('name_12'),
-('name_13'),
-('name_14'),
-('name_15'),
-('name_16'),
-('name_17'),
-('name_18'),
-('name_19'),
-('name_20'),
-('name_21'),
-('name_22'),
-('name_23'),
-('name_24'),
-('name_25'),
-('name_26'),
-('name_27'),
-('name_28'),
-('name_29'),
-('name_30'),
-('name_31'),
-('name_32'),
-('name_33'),
-('name_34'),
-('name_35'),
-('name_36'),
-('name_37'),
-('name_38'),
-('name_39'),
-('name_40'),
-('name_41'),
-('name_42'),
-('name_43'),
-('name_44'),
-('name_45'),
-('name_46'),
-('name_47'),
-('name_48'),
-('name_49'),
-('name_50'),
-('name_51'),
-('name_52'),
-('name_53'),
-('name_54'),
-('name_55'),
-('name_56'),
-('name_57'),
-('name_58'),
-('name_59'),
-('name_60'),
-('name_61'),
-('name_62'),
-('name_63'),
-('name_64'),
-('name_65'),
-('name_66'),
-('name_67'),
-('name_68'),
-('name_69'),
-('name_70');
+VALUES ('name_1'), ('name_2'), ('name_3'), ('name_4'),
+('name_5'), ('name_6'), ('name_7'), ('name_8'), 
+('name_9'), ('name_10'), ('name_11'), ('name_12'), 
+('name_13'), ('name_14'), ('name_15'), ('name_16'),
+('name_17'), ('name_18'), ('name_19'), ('name_20'),
+('name_21'), ('name_22'), ('name_23'), ('name_24'),
+('name_25'), ('name_26'), ('name_27'), ('name_28'),
+('name_29'), ('name_30'), ('name_31'), ('name_32'),
+('name_33'), ('name_34'), ('name_35'), ('name_36'),
+('name_37'), ('name_38'), ('name_39'), ('name_40'),
+('name_41'), ('name_42'), ('name_43'), ('name_44'),
+('name_45'), ('name_46'), ('name_47'), ('name_48'), 
+('name_49'), ('name_50'), ('name_51'), ('name_52'), 
+('name_53'), ('name_54'), ('name_55'), ('name_56'), 
+('name_57'), ('name_58'), ('name_59'), ('name_60'), 
+('name_61'), ('name_62'), ('name_63'), ('name_64'), 
+('name_65'), ('name_66'), ('name_67'), ('name_68'), 
+('name_69'), ('name_70');
 
-/*Таблица salary*/
+Таблица salary
 
-/*3) Создать таблицу salary
+3) Создать таблицу salary
 - id. Serial  primary key,
-- monthly_salary. Int, not null */
+- monthly_salary. Int, not null
 
 CREATE TABLE salary(
-	ID SERIAL PRIMARY KEY,
+	id SERIAL PRIMARY KEY,
 	monthly_salary INT NOT NULL
 );
 
-/*4) Наполнить таблицу salary 15 строками
-- 1000
-- 1100
-- 1200
-- 1300
-- 1400
-- 1500
-- 1600
-- 1700
-- 1800
-- 1900
-- 2000
-- 2100
-- 2200
-- 2300
-- 2400
-- 2500 */
+4) Наполнить таблицу salary 15 строками
 
 INSERT INTO salary (monthly_salary)
-VALUES (1100),
-(1200),
-(1300),
-(1400),
-(1500),
-(1600),
-(1700),
-(1800),
-(1900),
-(2000),
-(2100),
-(2200),
-(2300),
-(2400),
-(2500);
+VALUES (1100), (1200), (1300), (1400),
+(1500), (1600), (1700), (1800),
+(1900), (2000), (2100), (2200),
+(2300), (2400), (2500);
 
-/*5) Создать таблицу employee_salary
+5) Создать таблицу employee_salary
 - id. Serial  primary key,
 - employee_id. Int, not null, unique
-- salary_id. Int, not null */
+- salary_id. Int, not null
 
 CREATE TABLE employee_salary(
-	ID SERIAL PRIMARY KEY,
+	id SERIAL PRIMARY KEY,
 	employee_id INT UNIQUE NOT NULL,
 	salary_id INT UNIQUE NOT NULL
 );
 
+6) Наполнить таблицу employee_salary 40 строками:
+- в 10 строк из 40 вставить несуществующие employee_id
+
+INSERT INTO employee_salary (employee_id, salary_id)
+VALUES (16, 4), (28, 8), (44, 12), (71, 16),
+(55,3), (9, 7), (57, 11), (83, 15),
+(69, 2), (14, 6), (33, 10), (74, 14),	   
+(43, 1), (45, 16), (61, 9), (98, 13),	   
+(1, 16), (34, 4), (25, 8), (73, 12),
+(8, 15), (19, 3), (36, 7), (95, 11),
+(65, 14), (3, 2), (18, 6), (88, 10),
+(48, 13), (24, 1), (27, 5), (77, 9),	   
+(22, 12), (38, 16), (64, 4), (81, 8),	   
+(11, 11), (5, 15), (29, 3), (75, 7);	
+
+Таблица roles
+
+7) Создать таблицу roles
+- id. Serial  primary key,
+- role_name. int, not null, unique
+
+CRETE TABLE roles(
+	id SERIAL PRIMARY KEY,
+	role_name INT UNIQUE NOT NULL
+);
+
+8) Поменять тип столба role_name с int на varchar(30)
+
+ALTER TABLE roles
+ALTER column role_name TYPE VARCHAR(30);
+
+9) Наполнить таблицу roles 20 строками:
+
+INSERT INTO roles_n(role_name)
+VALUES ('Junior Python developer'),
+('Middle Python developer'),
+('Senior Python developer'),
+('Junior Java developer'),
+('Middle Java developer'),
+('Senior Java developer'),
+('Junior JavaScript developer'),
+('Middle JavaScript developer'),
+('Senior JavaScript developer'),
+('Junior Manual QA engineer'),
+('Middle Manual QA engineer'),
+('Senior Manual QA engineer'),
+('Project Manager'),
+('Designer'),
+('HR'),
+('CEO'),
+('Sales manager'),
+('Junior Automation QA engineer'),
+('Middle Automation QA engineer'),
+('Senior Automation QA engineer');
+
+Таблица roles_employee
+
+10) Создать таблицу roles_employee
+- id. Serial  primary key,
+- employee_id. Int, not null, unique (внешний ключ для таблицы employees, поле id)
+- role_id. Int, not null (внешний ключ для таблицы roles, поле id)
+
+CREATE TABLE roles_employee(
+	id SERIAL PRIMARY KEY,
+	employee_id INT UNIQUE NOT NULL,
+	role_id int not null,
+	foreign key (employee_id)
+		references employees(id),
+	foreign key (role_id)
+		references roles_n(id)
+);
+
+11) Наполнить таблицу roles_employee 40 строками:
+
+INSERT INTO roles_employee (employee_id, role_id)
+VALUES (2, 3), (4, 5), (8, 7), (10, 9),
+	   (13,11), (16, 13), (19, 15), (45, 12),
+	   (20, 17), (23, 19), (26, 1), (28, 4),	   
+	   (31, 6), (32, 8), (33, 10), (36, 13),	   
+	   (38, 16), (41, 4), (43, 8), (44, 12),
+	   (47, 14), (49, 18), (50, 20), (52, 2),
+	   (53, 11), (55, 5), (56, 6), (57, 10),
+	   (59, 13), (60, 1), (61, 8), (62, 9),	   
+	   (63, 12), (65, 16), (66, 4), (67, 8),	   
+	   (69, 11), (7, 15), (27, 3), (70, 7);
