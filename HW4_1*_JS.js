9faef7088baea11d26d4b4a7a6a350c3c0987f24
sedykhaleks <106698556+sedykhaleks@users.noreply.github.com>
2022-07-05 18:19:33 +0300

    Create HW4_1*_JS.js

---
+++ HW4_1*_JS.js
@@ -0,0 +1,21 @@
+/* Задания со *:
+1*: Преобразовать написанный код в 26-33 пунктах в функцию, принимающую на вход возраст.
+Пример: const checkAge = function(age) {
+Ваши преобразования
+}
+Вывести в консоль результат работы функции с возрастами 17, 18, 61 */
+
+checkAge = function(age_1, age_2, age_3) {
+    if (age_1 < age_2) {
+        console.log("You don’t have access cause your age is " + age_1 + ". " + "It’s less then " + age_2);
+    } else if (age_1 >=  age_2 && age_1 <  age_3) {
+        console.log("Welcome  !")
+    } else if (age_1  > age_3) {
+        console.log("Keep calm and look Culture channel")
+    } else {
+        console.log("Technical work")
+    }
+    
+    }
+
+checkAge(17, 18, 61)
