21c28a1eec03d441885b244834d1d851a039dab6
sedykhaleks <106698556+sedykhaleks@users.noreply.github.com>
2022-07-19 20:48:57 +0300

    Update tutorial.js

--- JavaScript/tutorial.js
+++ JavaScript/tutorial.js
@@ -6,3 +6,47 @@ var drawCats = function(howManyTimes) {
     }
 };
 drawCats(10);
+
+// Сколько секунд в часе?
+
+var secondsInAMinute = 60;
+var minutesInAnHour = 60;
+var secondsInAnHour = secondsInAMinute * minutesInAnHour;
+console.log(secondsInAnHour);
+
+var hoursInADay = 24;
+var secondsInADay = hoursInADay * secondsInAnHour;
+console.log(secondsInADay);
+
+var daysInAYear = 365;
+var secondsInAYear = secondsInADay * daysInAYear;
+console.log(secondsInAYear);
+
+var age = 34;
+console.log(age * secondsInAYear);
+
+// Инкремент +1 и декремент -1
+
+var highFives = 0;
+++highFives;
+console.log(highFives);
+
+var highFives1 = 0;
+highFives1++;
+console.log(highFives1);
+
+// Score +=  -=
+
+var x = 10;
+x += 5; // x = x + 5; 
+console.log(x);
+
+x -= 10; // x = x - 10;
+console.log(x);
+
+var balloons = 100;
+balloons *= 2;
+console.log(balloons);
+
+balloons /= 4;
+console.log(balloons);
