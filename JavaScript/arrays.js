74f2886642ecb56b7d2180b14ea43ae5dc5d6605
sedykhaleks <106698556+sedykhaleks@users.noreply.github.com>
2022-07-20 16:12:13 +0300

    Create arrays.js

---
+++ JavaScript/arrays.js
@@ -0,0 +1,160 @@
+// Создание массива
+
+var dinosaurs = [
+    "Тирранозавр",
+    "Велоцираптор",
+    "Стегозавр",
+    "Трицераптос",
+    "Брахиозавр",
+    "Птеранодон",
+    "Апатозавр",
+    "Диплодок",
+    "Компсогнат"
+];
+
+// Доступ к элементу
+
+console.log(dinosaurs[0]);
+console.log(dinosaurs[5]);
+
+//Создание и изменение элементов
+
+dinosaurs[0] = "Тирранозавр рекс";
+console.log(dinosaurs[0]);
+
+// Добавлять элементы
+
+dinosaurs[33] = "Филосораптор";
+dinosaurs[9] = 9;
+
+
+console.log(dinosaurs);
+
+var dinosaursAndNum = [
+    3, 18, "Dinos", 
+    ["трицератопс", "стегозавр", 3456.9],
+    10, 678, 88
+];
+
+console.log(dinosaursAndNum);
+
+// Как добраться до стегозавра?
+
+console.log(dinosaursAndNum[3][1]);
+
+// Как добраться до трицератопса
+
+console.log(dinosaursAndNum[3][0]);
+
+// Узнать длину массива .lenght
+
+console.log(dinosaursAndNum.length);
+console.log(dinosaurs.length);
+
+console.log([dinosaursAndNum.length - 1]);
+console.log([dinosaurs.length - 1]);
+
+// Добавление элемента в массив .push .unshift
+
+var animals = [];
+animals.push("Cat");
+animals.push("Dog");
+animals.push("Lama");
+
+console.log(animals.length);
+console.log(animals);
+console.log(animals[0]);
+
+animals.unshift("Monkey");
+
+console.log(animals.length);
+console.log(animals);
+console.log(animals[0]);
+
+animals.unshift("White Bear");
+
+console.log(animals.length);
+console.log(animals);
+console.log(animals[0]);
+
+// Удаление элементов массива .pop() .shift()
+
+var lastAnimal = animals.pop();
+
+console.log(lastAnimal);
+console.log(animals.length);
+console.log(animals);
+
+animals.unshift(lastAnimal);
+
+console.log(animals);
+
+var firstAnimal = animals.shift();
+console.log(firstAnimal);
+console.log(animals.length);
+console.log(animals);
+
+// Объединить массивы
+
+var furryAnimals = [
+    "Альпака", 
+    "Кольцехвостый лемур", 
+    "Йети"
+];
+var scalyAnimals = ["Удав", "Годзилла"];
+
+console.log(furryAnimals);
+console.log(scalyAnimals);
+
+var furryAndScalyAnimals = furryAnimals.concat(scalyAnimals);
+console.log(furryAndScalyAnimals);
+
+// Объединение нескольких массивов
+
+var furryAnimals = [
+    "Альпака", 
+    "Кольцехвостый лемур", 
+    "Йети"
+]; 
+var scalyAnimals = ["Удав", "Годзилла"];
+var featheredAnimals = ["Ара", "Додо"];
+
+var allAnimals = furryAnimals.concat(scalyAnimals, featheredAnimals);
+
+console.log(furryAnimals);
+console.log(scalyAnimals);
+console.log(featheredAnimals);
+console.log(allAnimals);
+
+// Поиск индекса элемента в массиве
+
+var colors = ["красный", "зеленый", "синий"];
+console.log(colors.lastIndexOf("красный"));
+console.log(colors.lastIndexOf("зеленый"));
+console.log(colors.lastIndexOf("синий"));
+console.log(colors.lastIndexOf("фиолетовый")); // вернет -1
+
+console.log(colors[0]);
+console.log(colors[1]);
+console.log(colors[2]);
+
+// Превратить массив в строку .join()
+
+var boringAnimals = [
+    "Мартышка", 
+    "Кот", 
+    "Рыба", 
+    "Ящерица"
+];
+
+console.log(boringAnimals.join());
+console.log(boringAnimals.join(", "));
+
+var myNames = [
+    "Николас", 
+    "Эндрю", 
+    "Максвелл", 
+    "Морган"
+];
+
+console.log(myNames.join(" "));
