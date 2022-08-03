7c34f06278ebe7714a54ea263f53896dde62ec89
sedykhaleks <106698556+sedykhaleks@users.noreply.github.com>
2022-08-03 16:28:15 +0300

    Update if_else.js

--- JavaScript/if_else.js
+++ JavaScript/if_else.js
@@ -13,3 +13,19 @@ if (lemonChicken) {
 } else {
     console.log("Что ж, остается рис с яйцом.");
 }
+
+// Напишите программу с переменной name. Если в этой переменной находится ваше имя, напечатайте: «Hi name!» — иначе напечатайте: 
+// «Hi, stranger!» Затем дополните программу, чтобы она здоровалась с вашим папой, если в name его имя, и с вашей мамой, 
+// если в name ее имя. Если же там что-то иное, по-прежнему печатайте «Привет, незнакомец!».
+
+var name = "Olga";
+
+if (name === "Aleksandr") {
+    console.log("Hi, " + name + "!");
+} else if (name === "Roman") {
+    console.log("Hi, my father " + name + "!");
+} else if (name === "Nina") {
+    console.log("Hi, my mother " + name + "!");
+} else {
+    console.log("Hi, stranger!");
+}
