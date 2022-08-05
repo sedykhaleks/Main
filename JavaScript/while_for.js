6c2cd6bb54750a9e1354b1684a416e2c7386e7ad
sedykhaleks <106698556+sedykhaleks@users.noreply.github.com>
2022-08-05 10:56:15 +0300

    Update and rename while.js to while_for.js

---
+++ JavaScript/while_for.js
@@ -0,0 +1,56 @@
+// цикл while
+
+var sheepCounter = 0;
+while (sheepCounter < 10) {
+    console.log("Counted sheeps: " + sheepCounter + " !");
+    sheepCounter++;
+}
+console.log("Hrrrrrrr-pssssss");
+
+// цикл for
+
+for (var sheepCounted = 0; sheepCounted < 10; sheepCounted++) {
+    console.log("Counted sheeps: " + sheepCounted + " !");
+}
+console.log("Hrrrrrrr-pssssss");
+
+var timesToSayHello = 3;
+for (var i = 0; i < timesToSayHello; i++) {
+    console.log("Hello !");
+}
+
+// цикл for массивы и строки
+
+// обращение к элементам массива
+
+var animals = ["lion", "flamengo", "white bear", "shark"];
+for (var i = 0; i < animals.length; i++) {
+    console.log("The zoo has a " + animals[i] + ".");
+}
+
+//обращение к отдельным символам строки
+
+var myName = "Aleksandr";
+for (var i = 0; i < myName.length; i++) {
+  console.log("My name has a letter " + myName[i] + ".");
+}
+
+// напечатать все степени 2 не превышающие числа 10000
+
+for (var x = 2; x < 10000; x = x * 2) {
+    console.log(x);
+}
+
+// написать цикл for который печатает степени тройки на превышающие 10000
+
+for (var x = 3; x < 10000; x = x * 3) {
+    console.log(x);
+}
+
+// перепишите цикл for используя цикл while
+
+var x = 1;
+while (x < 10000) {
+    x = x * 3;
+    console.log(x);
+}
